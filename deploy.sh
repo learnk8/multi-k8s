b638f80e5ba68cd7b87e9015ef7440af3e18a91f
Mohammad Mahboubian <m_mahboubian@yahoo.com>
2020-08-23 01:08:20 +0800

    changed deploy

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,10 @@
 # docker push stephengrider/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
+# kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
+# kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
+# kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
+
+kubectl set image deployments/server-deployment server=stephengrider/multi-server:latest
+kubectl set image deployments/client-deployment client=stephengrider/multi-client:latest
+kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:latest
